8f74c3c584a866333581db4caf54a561df471bfd
Andrew Hexen <SyberHexen@gmail.com>
2022-02-02 06:25:39 -0800

    nairo: add vendor interface for android.hardware.biometrics.fingerprint
    Signed-off-by: Andrew Hexen <SyberHexen@gmail.com>
    Change-Id: I59b0e366dbb5ed59e1ac42f0604f7939da88f9b0

--- device.mk
+++ device.mk
@@ -58,6 +58,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/uinput-egis.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-egis.kl \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl
 
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1.vendor
+
 # FM
 PRODUCT_PACKAGES += \
     FM2 \
