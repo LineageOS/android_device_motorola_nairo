43fd58aa41b56b8ad11dd23e5dcc5b20d4c37fa5
LuK1337 <priv.luk@gmail.com>
2023-06-22 22:34:55 +0530

    nairo: Include vendor/debugfs.config
    This disables the following kernel configs:
    * CONFIG_DEBUG_FS
    * CONFIG_PAGE_OWNER
    
    Change-Id: Idbf74e3dee4dccff4708462271e79374e28449ae

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -29,7 +29,7 @@ BOARD_HAS_QCA_FM_SOC := "cherokee"
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 
 # Kernel
-TARGET_KERNEL_CONFIG := vendor/nairo_defconfig
+TARGET_KERNEL_CONFIG := vendor/nairo_defconfig vendor/debugfs.config
 TARGET_KERNEL_ADDITIONAL_FLAGS += \
     DTC_PREBUILT=true \
     DTC=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/dtc/dtc \
