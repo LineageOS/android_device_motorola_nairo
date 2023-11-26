17b3e25d6376da375e2857e325cedbeb908aaa9d
Andrew Hexen <SyberHexen@gmail.com>
2023-11-26 05:31:39 -0800

    nairo: update refresh rate settings
    * Nairo has dynamic refresh rate unlike other moto-sm7250
    * Simplify overlays and remove unnecessary items
    * Kang surfaceflinger timers from moto-sm7325-common.
    
    Signed-off-by: Andrew Hexen <SyberHexen@gmail.com>
    Change-Id: I8a257ec02aa893c69a1c743519791569bc1ead51

--- properties.mk
+++ properties.mk
@@ -41,8 +41,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.sf.color_mode=0 \
 
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.surface_flinger.set_idle_timer_ms=4000 \
-    ro.surface_flinger.set_touch_timer_ms=4000 \
+    ro.surface_flinger.set_idle_timer_ms=250 \
+    ro.surface_flinger.set_touch_timer_ms=1000 \
     ro.surface_flinger.set_display_power_timer_ms=1000 \
     ro.surface_flinger.use_smart_90_for_video=true \
     ro.surface_flinger.refresh_rate_switching=true
