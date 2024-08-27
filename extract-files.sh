faf968ba26330851c64b62b54521ae46b6459478
Michael Bestas <mkbestas@lineageos.org>
2024-08-27 22:05:28 +0300

    nairo: Sync extract scripts with templates
    Change-Id: I98a36854c5a0c0390592911bb2558b34eff77efc

--- extract-files.sh
+++ extract-files.sh
@@ -1,40 +1,54 @@
 #!/bin/bash
 #
-# Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2020 The LineageOS Project
-#
+# SPDX-FileCopyrightText: 2016 The CyanogenMod Project
+# SPDX-FileCopyrightText: 2017-2024 The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 #
 
 function blob_fixup() {
     case "${1}" in
-    # Patch configureRpcThreadpool
-    vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so)
-        hexdump -ve '1/1 "%.2X"' "${2}" | sed "s/8A0A0094/1F2003D5/g" | xxd -r -p > "${EXTRACT_TMP_DIR}/${1##*/}"
-        mv "${EXTRACT_TMP_DIR}/${1##*/}" "${2}"
-        ;;
-    # memset shim
-    vendor/bin/charge_only_mode)
-        "${PATCHELF}" --add-needed libmemset_shim.so "${2}"
-        ;;
-    # rename moto modified tinyalsa
-    vendor/lib/libtinyalsa-moto.so)
-        "${PATCHELF}" --set-soname libtinyalsa-moto.so "${2}"
-        ;;
-    # rename moto modified tinyalsa
-    vendor/lib/hw/sound_trigger.primary.lito.so | vendor/lib/soundfx/libmmieffectswrapper.so | vendor/lib/soundfx/libspeakerbundle.so)
-        "${PATCHELF}" --replace-needed libtinyalsa.so libtinyalsa-moto.so "${2}"
-        ;;
-    # rename moto modified primary audio to not conflict with source built
-    vendor/lib/hw/audio.primary.lito-moto.so)
-        "${PATCHELF}" --set-soname audio.primary.lito-moto.so "${2}"
-        "${PATCHELF}" --replace-needed libtinyalsa.so libtinyalsa-moto.so "${2}"
-        ;;
-    # __lttf2 shim
-    vendor/lib64/libvidhance.so)
-        "${PATCHELF}" --print-needed "${2}" |grep -q libcomparetf2_shim || "${PATCHELF}" --add-needed libcomparetf2_shim.so "${2}"
-        ;;
+        # Patch configureRpcThreadpool
+        vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so)
+            [ "$2" = "" ] && return 0
+            hexdump -ve '1/1 "%.2X"' "${2}" | sed "s/8A0A0094/1F2003D5/g" | xxd -r -p > "${EXTRACT_TMP_DIR}/${1##*/}"
+            mv "${EXTRACT_TMP_DIR}/${1##*/}" "${2}"
+            ;;
+        # memset shim
+        vendor/bin/charge_only_mode)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF}" --add-needed libmemset_shim.so "${2}"
+            ;;
+        # rename moto modified tinyalsa
+        vendor/lib/libtinyalsa-moto.so)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF}" --set-soname libtinyalsa-moto.so "${2}"
+            ;;
+        # rename moto modified tinyalsa
+        vendor/lib/hw/sound_trigger.primary.lito.so | vendor/lib/soundfx/libmmieffectswrapper.so | vendor/lib/soundfx/libspeakerbundle.so)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF}" --replace-needed libtinyalsa.so libtinyalsa-moto.so "${2}"
+            ;;
+        # rename moto modified primary audio to not conflict with source built
+        vendor/lib/hw/audio.primary.lito-moto.so)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF}" --set-soname audio.primary.lito-moto.so "${2}"
+            "${PATCHELF}" --replace-needed libtinyalsa.so libtinyalsa-moto.so "${2}"
+            ;;
+        # __lttf2 shim
+        vendor/lib64/libvidhance.so)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF}" --print-needed "${2}" |grep -q libcomparetf2_shim || "${PATCHELF}" --add-needed libcomparetf2_shim.so "${2}"
+            ;;
+        *)
+            return 1
+            ;;
     esac
+
+    return 0
+}
+
+function blob_fixup_dry() {
+    blob_fixup "$1" ""
 }
 
 # If we're being sourced by the common script that we called,
