fa231bca681c2f2ddc53d84abf655b440c745833
Giovanni Ricca <giovanniricca@proton.me>
2025-03-08 22:35:43 +0100

    nairo: Simplify rro overlays
    Change-Id: Id0e3a2c86bae8531d346a9bc332a4117ecbbe60a

--- device.mk
+++ device.mk
@@ -31,10 +31,7 @@ PRODUCT_PACKAGES += \
 
 # SKU specific RROs
 PRODUCT_PACKAGES += \
-    LineageSystemUIVariant1 \
-    LineageSystemUIVariant2 \
-    LineageSystemUIVariant3 \
-
+    LineageSystemUIXT2075-3
 
 # Audio
 PRODUCT_PACKAGES += \
