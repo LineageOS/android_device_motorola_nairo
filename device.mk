cee4ea54116b72b7699fe02c278cdda8d7d6d4ad
basamaryan <basam.aryan@gmail.com>
2025-03-08 00:21:48 -0500

    nairo: Remove non-existant properties.mk include
    Change-Id: Ife4173129bd74cae14305e4f065778f3b131d1dd

--- device.mk
+++ device.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2017-2020 The LineageOS Project
+# Copyright (C) 2017-2025 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -18,9 +18,6 @@ $(call inherit-product, device/motorola/sm7250-common/common.mk)
 
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/motorola/nairo/nairo-vendor.mk)
-
-# Properties
--include $(LOCAL_PATH)/properties.mk
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
