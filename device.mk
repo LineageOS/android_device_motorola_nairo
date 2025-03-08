21686cda98895b1c601738cbe0136f24f19e3c27
Giovanni Ricca <giovanniricca@proton.me>
2025-03-08 22:22:35 +0100

    nairo: configs: Move media folder out of configs
    Change-Id: Ie4c18335cfd1278f3196e3f8bceee00c009648ff

--- device.mk
+++ device.mk
@@ -75,7 +75,7 @@ PRODUCT_PACKAGES += \
 
 # Media
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/media/media_profiles_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_vendor.xml
+    $(LOCAL_PATH)/media/media_profiles_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_vendor.xml
 
 # Power
 PRODUCT_COPY_FILES += \
