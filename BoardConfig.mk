8e12b4459ef59db3feec2b62722f81b0578e9082
Giovanni Ricca <giovanniricca@proton.me>
2025-03-09 15:19:49 +0100

    nairo: Build audio & soundtrigger HALs from source
    Change-Id: I51c5935ebbe2988a8e484d024857864a6c353ce7

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -8,6 +8,11 @@ include device/motorola/sm7250-common/BoardConfigCommon.mk
 
 DEVICE_PATH := device/motorola/nairo
 
+# Audio
+AUDIO_FEATURE_ENABLED_EXT_AMPLIFIER := true
+AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT := true
+BOARD_SUPPORTS_OPENSOURCE_STHAL := true
+
 # Display
 TARGET_SCREEN_DENSITY := 420
 
